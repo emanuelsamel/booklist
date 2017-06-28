c6db4c087536ec32697eec9b8c9eda4a0d54f74a
emanuelsamel <mannys94@hotmail.com>
2017-06-27 22:05:11 -0400

    controller done

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,20 @@
 class BooksController < ApplicationController
 
-def index
-@books = Book.all
-respond_to do |format|
-  format.html
-end
-end
+  def index
+    @books = Book.all
+    respond_to do |format|
+      format.html
+      format.text
+
+      format.csv do
+        render csv: @books
+      end
+
+      format.json do
+        render json: @books
+      end
+
+    end
+  end
+
 end
